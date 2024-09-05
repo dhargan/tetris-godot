c8a5af475591f16fd66d440afd01e168a0fe0565
dhargan <teomanunay@gmail.com>
2024-09-05 16:13:52 +0300

    Add movement base for x axis

--- scenes/blocks/tetrominos/tetromino.gd
+++ scenes/blocks/tetrominos/tetromino.gd
@@ -6,8 +6,26 @@ const MAX_SPEED = 5000.0
 var current_speed = BASE_SPEED
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("move_down"):
+	move_down(Input.is_action_pressed("move_down"), delta)
+	
+	if Input.is_action_just_pressed("move_right"):
+		move_right()
+		
+	if Input.is_action_just_pressed("move_left"):
+		move_left()
+	
+	
+func move_down(speed_up: bool, delta: float) -> void:
+	if speed_up:
 		current_speed = MAX_SPEED
 	else:
 		current_speed = BASE_SPEED
+		
 	position.y += current_speed * delta
+
+
+func move_right():
+	position.x += 32
+	
+func move_left():
+	position.x -= 32
