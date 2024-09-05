dc8199c999747d33ef0aee61c4a2aed30f5e740e
dhargan <teomanunay@gmail.com>
2024-09-05 15:54:55 +0300

    Add move_down action

--- scenes/blocks/tetrominos/tetromino.gd
+++ scenes/blocks/tetrominos/tetromino.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
-var SPEED = 40.0
+const BASE_SPEED = 40.0
+const MAX_SPEED = 5000.0
+
+var current_speed = BASE_SPEED
 
 func _process(delta: float) -> void:
-	position.y += SPEED * delta
+	if Input.is_action_pressed("move_down"):
+		current_speed = MAX_SPEED
+	else:
+		current_speed = BASE_SPEED
+	position.y += current_speed * delta
