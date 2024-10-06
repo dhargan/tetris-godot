85a30e8d3b48ba6285c26a338739f427661ecc93
dhargan <teomanunay@gmail.com>
2024-10-06 19:50:52 +0300

    Add tetromino manager

--- scenes/managers/tetromino_manager.gd
+++ scenes/managers/tetromino_manager.gd
@@ -1,3 +1,17 @@
 extends Node2D
 
 @export var tetrominos: Array[PackedScene] = []
+
+var current_tetromino = null
+
+func _ready() -> void:
+	spawn_tetromino()
+
+func _process(delta: float) -> void:
+	if current_tetromino and current_tetromino.is_static:
+		spawn_tetromino()
+
+func spawn_tetromino() -> void:
+	var tetromino_index = randi_range(0, 6)
+	current_tetromino = tetrominos[tetromino_index].instantiate() as Node2D
+	add_child(current_tetromino)
